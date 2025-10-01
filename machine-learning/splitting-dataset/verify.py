9eb0944585f975d99b40291d7d7b42271dca91fc
Connor Ford <connor.ford@dsu.edu>
2025-10-01 11:57:49 -0500

    rewrote challenge verify to handle new paceAITester updates

--- machine-learning/splitting-dataset/verify.py
+++ machine-learning/splitting-dataset/verify.py
@@ -1,130 +1,206 @@
+"""Verification script for Splitting Datasets challenge."""
+
 #!/usr/bin/exec-suid -- /usr/bin/python3.12 -I
 import sys
-sys.path.append('/challenge')
 
-from paceAITester.datatypes import FunctionCall
-from paceAITester.verify_helpers import *
-from typing import Tuple
+sys.path.append("/challenge")
+
+from dataclasses import dataclass
+from typing import Any, Optional, Tuple
+from paceAITester.datatypes import AssignStatement, FunctionCallStatement
+from paceAITester.parser import StatementParser
+from paceAITester.utils import (
+    find_function_calls,
+    function_not_called,
+    run_verification,
+)
+
+
+@dataclass
+class UserVariables:
+    """Stores the names of user variables"""
+
+    x: Optional[Any] = None
+    y: Optional[Any] = None
+    x_train: Optional[Any] = None
+    x_test: Optional[Any] = None
+    y_train: Optional[Any] = None
+    y_test: Optional[Any] = None
 
 
 class Validator:
+    """Validator class that runs verification checks on user submitted code for
+    challenge.
+    """
+
     def __init__(self, script_path: str):
-        self.lines = extract_python_details(script_path)
-        self.script_path = script_path
+        self.parser = StatementParser(script_path)
+        self.lines = self.parser.parse()
+        self.user_vars = UserVariables()
+        self.checks = [self.check_x, self.check_y, self.check_split]
 
-        # User variables
-        self.X = None
-        self.y = None
-        self.X_train = None
-        self.X_test = None
-        self.y_train = None
-        self.y_test = None
+    def check_x(self) -> Tuple[bool, str]:
+        """Step Goal: Create X variable using np.random.rand() which has 500 samples and
+        7 features.
 
-        self.checks = [self.check_X, self.check_y, self.check_split]
-    
-    def check_X(self) -> Tuple[bool, str]:
+        Returns:
+            Tuple[bool, str]: A tuple containing a boolean indicating success or
+                failure of the validation, and a string message providing error details
+                if failure.
         """
-        Step Goal: Create X variable using np.random.rand() which has 500 samples and 7 features.
+        function_name = "np.random.rand"
+        function_calls = find_function_calls(self.lines, function_name)
 
-        :return: A tuple containing a boolean indicating success or failure of the validation,
-                and a string message providing error details if failure.
-        :rtype: tuple[bool, str]
-        """
-        function_name = 'np.random.rand'
-        function_calls = find_function_call(self.lines, function_name)
-
+        error_msg = None
         if function_not_called(function_calls):
-            return False, f"{function_name}() isn't called"
-        if len(function_calls) > 1:
-            return False, f"{function_name}() should only be called once"
-        
-        function_call = FunctionCall.from_dict(function_calls[0])
+            error_msg = f"{function_name}() isn't called"
+        elif len(function_calls) > 1:
+            error_msg = f"{function_name}() should only be called once"
+        elif not isinstance(function_calls[0], AssignStatement):
+            error_msg = (
+                "Make sure you're storing the output of "
+                + f"{function_name}() in a variable"
+            )
+        else:
+            function_call: AssignStatement = function_calls[0]
 
-        if function_call.variable is None:
-            return False, f"Make sure you're storing the output of {function_name}() in a variable"
-    
-        self.X = function_call.variable
+            if len(function_call.targets) != 1:
+                error_msg = (
+                    f"The output of {function_name}() should only be stored in a "
+                    + "single variable"
+                )
+            elif function_call.value.args != ["500", "7"]:
+                error_msg = (
+                    "Did you pass the correct values in the instructions "
+                    + f"to {function_name}()?"
+                )
 
-        if function_call.args != ['500', '7']:
-            return False, f'Did you pass the correct values in the instructions to {function_name}()?'
-        
-        return True, ''
+        if error_msg:
+            return False, error_msg
+
+        self.user_vars.x = function_call.targets[0]
+
+        return True, ""
 
     def check_y(self) -> Tuple[bool, str]:
+        """Step Goal: Create y using np.random.randint() which has the same amount of
+        samples and outputs either 0 or 1.
+
+        Returns:
+            Tuple[bool, str]: A tuple containing a boolean indicating success or
+                failure of the validation, and a string message providing error details
+                if failure.
         """
-        Step Goal: Create y using np.random.randint() which has the same amount of samples and outputs either 0 or 1.
+        function_name = "np.random.randint"
+        statements = find_function_calls(self.lines, function_name)
 
-        :return: A tuple containing a boolean indicating success or failure of the validation,
-                and a string message providing error details if failure.
-        :rtype: tuple[bool, str]
-        """
-        function_name = 'np.random.randint'
-        function_calls = find_function_call(self.lines, function_name)
+        error_msg = None
+        if function_not_called(statements):
+            error_msg = f"{function_name}() isn't called"
+        elif len(statements) > 1:
+            error_msg = f"{function_name}() should only be called once"
+        elif not isinstance(statements[0], AssignStatement):
+            error_msg = (
+                "Make sure you're storing the output of "
+                + f"{function_name}() in a variable"
+            )
+        else:
+            statement: AssignStatement = statements[0]
+            if isinstance(statement.value, FunctionCallStatement):
+                function_call: FunctionCallStatement = statement.value
+            else:
+                raise TypeError(
+                    "Expected FunctionCallStatement, "
+                    + f"got {type(statement.value).__name__}"
+                )
 
-        if function_not_called(function_calls):
-            return False, f"{function_name}() isn't called"
-        if len(function_calls) > 1:
-            return False, f"{function_name}() should only be called once"
-        
-        function_call = FunctionCall.from_dict(function_calls[0])
+            if len(statement.targets) != 1:
+                error_msg = (
+                    f"The output of {function_name}() should only be stored in a "
+                    + "single variable"
+                )
+            elif function_call.args != ["0", "2"] or function_call.kwargs != {
+                "size": "500"
+            }:
+                error_msg = (
+                    "Did you pass the correct values in the instructions "
+                    + f"to {function_name}()?"
+                )
 
-        if function_call.variable is None:
-            return False, f"Make sure you're storing the output of {function_name}() in a variable"
-    
-        self.y = function_call.variable
+        if error_msg:
+            return False, error_msg
 
-        if function_call.args != ['0', '2'] or function_call.kwargs != {'size': '500'}:
-            return False, f'Did you pass the correct values in the instructions to {function_name}()?'
+        self.user_vars.y = statement.targets[0]
 
-        return True, ''
+        return True, ""
 
     def check_split(self) -> Tuple[bool, str]:
+        """Step Goal: Split the dataset where the train dataset is 70% of the original
+        dataset.
+
+        Returns:
+            Tuple[bool, str]: A tuple containing a boolean indicating success or
+                failure of the validation, and a string message providing error details
+                if failure.
         """
-        Step Goal: Split the dataset where the train dataset is 70% of the original dataset.
+        function_name = "train_test_split"
+        statements = find_function_calls(self.lines, function_name)
 
-        :return: A tuple containing a boolean indicating success or failure of the validation,
-                and a string message providing error details if failure.
-        :rtype: tuple[bool, str]
-        """
-        function_name = 'train_test_split'
-        function_calls = find_function_call(self.lines, function_name)
+        error_msg = None
+        if function_not_called(statements):
+            error_msg = f"{function_name}() isn't called"
+        elif len(statements) > 1:
+            error_msg = f"{function_name}() should only be called once"
+        elif not isinstance(statements[0], AssignStatement):
+            error_msg = (
+                "Make sure you're storing the output of train_test_split() "
+                + "in variables for your train and test datasets "
+                + "(HINT: It's 4 variables)"
+            )
+        else:
+            function_call = statements[0]
+            statement: AssignStatement = statements[0]
+            if isinstance(statement.value, FunctionCallStatement):
+                function_call: FunctionCallStatement = statement.value
+            else:
+                raise TypeError(
+                    "Expected FunctionCallStatement, "
+                    + f"got {type(statement.value).__name__}"
+                )
 
-        if function_not_called(function_calls):
-            return False, f"{function_name}() isn't called"
-        if len(function_calls) > 1:
-            return False, f'{function_name}() should only be called once'
-        
-        function_call = FunctionCall.from_dict(function_calls[0])
+            if len(statement.targets) != 4:
+                error_msg = "Incorrect number of output variables."
+            elif function_call.args != [self.user_vars.x, self.user_vars.y]:
+                error_msg = "Did you provide the correct variables for X and y?"
+            elif function_call.kwargs != {"test_size": "0.3"}:
+                error_msg = (
+                    "Did you correctly set the 'test_size' parameter so that the "
+                    + "training dataset is 70% of the original dataset?"
+                )
 
-        if function_call.variable is None:
-            return False, "Make sure you're storing the output of train_test_split() in variables for your train and test datasets (HINT: It's 4 variables)"
-        
-        if len(function_call.variable) != 4:
-            return False, 'Incorrect number of output variables.'
+        if error_msg:
+            return False, error_msg
 
-        self.X_train = function_call.variable[0]
-        self.X_test = function_call.variable[1]
-        self.y_train = function_call.variable[2]
-        self.y_test = function_call.variable[3]
+        self.user_vars.x_train = statement.targets[0]
+        self.user_vars.x_test = statement.targets[1]
+        self.user_vars.y_train = statement.targets[2]
+        self.user_vars.y_test = statement.targets[3]
 
-        if function_call.args != [self.X, self.y]:
-            return False, 'Did you provide the correct variables for X and y?'
-    
-        if function_call.kwargs != {'test_size': '0.3'}:
-            return False, "Did you correctly set the 'test_size' parameter so that the training dataset is 70% of the original dataset?"
-
-        return True, ''
+        return True, ""
 
     def verify_code(self) -> None:
+        """Runs the list of verification functions to make sure the user submitted code
+        is correct.
+        """
         run_verification(self.checks)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print('Usage: verify <user_script>.py')
+        print("Usage: verify <user_script>.py")
         sys.exit(1)
-    
-    script_path = sys.argv[1]
 
-    validator = Validator(script_path)
+    filepath = sys.argv[1]
+
+    validator = Validator(filepath)
     validator.verify_code()
